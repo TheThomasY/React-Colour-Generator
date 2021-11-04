1f075a5d080530dc9b01b6098a0d69b6908d6799
TheThomasY <twy1g16@soton.ac.uk>
2021-11-04 17:41:02 +0000

    empty set up done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import SearchBar from './components/SearchBar';
+import Types from './components/Typeography';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <div className='App'>
+      <SearchBar></SearchBar>
+      <Types></Types>
+    </div>
+  );
 }
 
 export default App;
