b1a5cd71f8de0b110f777942db027156952c98c9
TheThomasY <twy1g16@soton.ac.uk>
2021-11-05 18:07:14 +0000

    feat: search functioning, passing code through components, no colours

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import React, { useState } from 'react';
 import SearchBar from './components/SearchBar';
 import ColourGrid from './components/ColourGrid';
+import './App.css';
 
 function App() {
+  const [colourHex, setColourHex] = useState('');
+
+  const showColourGrid = (searchColourHex) => {
+    setColourHex(searchColourHex);     
+  }
+  
+
+
   return (
     <div className='App'>
-      <SearchBar />
-      <ColourGrid />
+      <SearchBar onSearchColour={showColourGrid}/>
+      <ColourGrid colourHex={colourHex}/>
     </div>
   );
 }
