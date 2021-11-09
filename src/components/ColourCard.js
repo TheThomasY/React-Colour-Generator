439a3fe388cea596dd1f5f9e91a174a9fc9ef579
TheThomasY <twy1g16@soton.ac.uk>
2021-11-09 17:10:46 +0000

    style: style clean up, added copied to clipboard popup message

--- src/components/ColourCard.js
+++ src/components/ColourCard.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function ColourCard(props) {
+  const [copied, setCopied] = useState(false);
+
+  const copyToClipboardHandler = () => {
+    navigator.clipboard.writeText(props.hex);
+    setCopied(true);
+
+    setTimeout(function () {
+      setCopied(false);
+    }, 1500);
+  };
+
   let textColour = props.percentage >= 0 ? '#ffffff' : '#000000';
-  console.log(textColour);
+
+  let copiedPopup = copied ? '1' : '0';
 
   return (
     <div
       style={{ backgroundColor: props.rgb, color: textColour }}
       className='colour-card'
-      onClick={() => navigator.clipboard.writeText(props.hex)}
     >
-      <h2 className='card-hex'>{props.hex}</h2>
+      <h2 className='card-hex' onClick={copyToClipboardHandler}>
+        {props.hex}
+      </h2>
       <h3 className='card-percentage'>{props.percentage}%</h3>
+      <div style={{ opacity: copiedPopup }} className='copy-popup'>
+        Copied to clipboard
+      </div>
     </div>
   );
 }
